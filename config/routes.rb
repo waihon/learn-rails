6e04d3beee00df0b4a4b96bdf4134b4b1062dde5
Waihon Yew <yewwaihon@gmail.com>
2016-06-04 13:12:58 +0800

    Mailing list

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   resources :contacts, only: [:new, :create]
+  resources :visitors, only: [:new, :create]
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
